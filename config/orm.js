2470bfa3ace9155e8ed1b1a1fdd01cc72ad64f7c
Philip Ross <philiplross@gmail.com>
2018-11-17 10:49:37 -0800

    v 3.1

---
+++ config/orm.js
@@ -0,0 +1,11 @@
+var connection = require("./config/connection.js");
+
+var orm = {
+
+
+// insert all ORM into here
+
+
+};
+
+module.exports = orm;
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var router = express.Router();
+
+var cat = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    cat.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+
+  
