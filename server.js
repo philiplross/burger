3ef2510b5b10817fd73ad9485806c406134788e4
Philip Ross <philiplross@gmail.com>
2018-11-17 12:53:14 -0800

    v 3.3

--- server.js
+++ server.js
@@ -4,14 +4,11 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -22,8 +19,6 @@ var routes = require("./controllers/catsController.js");
 
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
